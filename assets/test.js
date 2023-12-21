47e5db6ddab25fa7e75cbd91cdf89cbd163345bb
krypton99 <17520777@gm.uit.edu.vn>
2023-12-21 10:37:13 +0700

    build static homepage UI

---
+++ assets/test.js
@@ -0,0 +1,16 @@
+var a = false || 8 || 0;
+
+
+if(languages==undefined) {
+    var languages = [
+        'Javascript',
+        'Javascript',
+        'Javascript',
+    ]
+}
+
+sessionStorage = languages
+
+languages.push('PHP', 'Dart')
+
+console.log(sessionStorage);
